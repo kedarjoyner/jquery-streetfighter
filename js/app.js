8d47c42c6b89c19842ce7a3b7b6649b51112221a
kedarjoyner <kedarjoyner@gmail.com>
2015-09-16 16:10:13 -0500

    hadouken move

--- js/app.js
+++ js/app.js
@@ -1,11 +1,28 @@
+
+
 $(document).ready(function() {
 	$('.ryu-container').mouseenter(function() {
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
 	})
-	$('.ryu-container').mouseleave(function() {
+	.mouseleave(function() {
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
+	})
+	.mousedown(function() {
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').show().animate({
+			'left': '1020px'}, 500, function (){
+				$(this).hide();
+				$(this).css('left', '520px');	
+	});
+	})
+	.mouseup(function() {
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+		//ryu goes back to ready position
 	});
 });
+
 
